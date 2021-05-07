3d23afadafd18c2ddc5eeed446e17bd3ef4a0002
DeasyNabila <deasy_nabila_29rpl@student.smktelkom-mlg.sch.id>
2021-05-07 13:59:23 +0700

    Lat2

---
+++ src/Interface/Line.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interface;
+
+/**
+ *
+ * @author MOKLET-1
+ */
+public class Line implements Relation {
+
+    @Override
+    public boolean isGreater(Object a, Object b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isLess(Object a, Object b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+}
